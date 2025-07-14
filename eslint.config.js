901bf0cb98033580cbb0f709e728e197202e4068
Admin <admin@Admins-MacBook-Pro.local>
2025-07-14 23:17:42 +0100

    add tailwind and lint project

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,9 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'semi': ['error', 'always'],
+      'object-curly-spacing': ['error', 'always']
+    }
   },
 ])
