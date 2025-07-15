3a4b6abe57a098c105b2f4270f55460c13822f75
Daniel Waldow <daniel.waldow@risksmart.com>
2025-07-15 17:34:36 +0100

    port over hooks from old portfolio

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import reactESLintPlugin from 'eslint-plugin-react'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import reactESLintPlugin from 'eslint-plugin-react';
+import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -33,4 +33,4 @@ export default tseslint.config([
       ],
     }
   },
-])
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import glsl from 'vite-plugin-glsl';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    glsl(),
     react(),
     tailwindcss(),
   ],
